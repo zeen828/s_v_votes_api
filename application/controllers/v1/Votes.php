e454572f349fd37104c381d18737acd65f68df72
Will Lu <zeren828@gmail.com>
2017-08-07 17:34:21 +0800

    dev

---
+++ application/controllers/v1/Votes.php
@@ -0,0 +1,90 @@
+<?php
+defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
+ini_set ( "display_errors", "On" ); // On, Off
+require_once APPPATH . '/libraries/MY_REST_Controller.php';
+class Votes extends MY_REST_Controller {
+	private $data_debug;
+	private $data_result;
+	public function __construct() {
+		parent::__construct ();
+		$this->_my_logs_start = true;
+		$this->_my_logs_type = 'auth';
+		$this->data_debug = true;
+		// 資料庫
+		// $this->load->database ( 'vidol_billing_write' );
+		// 效能檢查
+		// $this->output->enable_profiler(TRUE);
+	}
+	public function __destruct() {
+		parent::__destruct ();
+		unset ( $this->data_debug );
+		unset ( $this->data_result );
+	}
+	public function vote_get() {
+		try {
+			// 開始時間標記
+			$this->benchmark->mark ( 'code_start' );
+			// 引入
+			$this->config->load ( 'restful_status_code' );
+			$this->lang->load ( 'restful_status_lang', 'traditional-chinese' );
+			// 變數
+			$data_input = array ();
+			$data_cache = array ();
+			$this->data_result = array (
+					'result' => array (),
+					'code' => $this->config->item ( 'system_default' ),
+					'message' => '',
+					'time' => 0 
+			);
+			// 接收變數
+			$data_input ['username'] = $this->get ( 'username' );
+			$data_input ['password'] = $this->get ( 'password' );
+			// 結束時間標記
+			$this->benchmark->mark ( 'code_end' );
+			// 標記時間計算
+			$this->data_result ['time'] = $this->benchmark->elapsed_time ( 'code_start', 'code_end' );
+			$this->response ( $this->data_result, 200 );
+		} catch ( Exception $e ) {
+			show_error ( $e->getMessage () . ' --- ' . $e->getTraceAsString () );
+		}
+	}
+	public function vote_post() {
+		try {
+			// 開始時間標記
+			$this->benchmark->mark ( 'code_start' );
+			// 引入
+			$this->config->load ( 'restful_status_code' );
+			$this->lang->load ( 'restful_status_lang', 'traditional-chinese' );
+			// 變數
+			$data_input = array ();
+			$data_cache = array ();
+			$this->data_result = array (
+					'result' => array (),
+					'code' => $this->config->item ( 'system_default' ),
+					'message' => '',
+					'time' => 0 
+			);
+			// 接收變數
+			$data_input ['username'] = $this->post ( 'username' );
+			$data_input ['password'] = $this->post ( 'password' );
+			// 必填檢查
+			if (empty ( $data_input ['username'] ) && empty ( $data_input ['password'] )) {
+				// 必填錯誤
+				$this->data_result ['message'] = $this->lang->line ( 'input_required_error' );
+				$this->data_result ['code'] = $this->config->item ( 'input_required_error' );
+				// 必填錯誤標記
+				$this->benchmark->mark ( 'error_required' );
+				$this->data_result ['time'] = $this->benchmark->elapsed_time ( 'code_start', 'error_required' );
+				$this->response ( $this->data_result, 416 );
+				return;
+			}
+			// 結束時間標記
+			$this->benchmark->mark ( 'code_end' );
+			// 標記時間計算
+			$this->data_result ['time'] = $this->benchmark->elapsed_time ( 'code_start', 'code_end' );
+			$this->response ( $this->data_result, 200 );
+		} catch ( Exception $e ) {
+			show_error ( $e->getMessage () . ' --- ' . $e->getTraceAsString () );
+		}
+	}
+}
