97b3b29d84f906a66d1bd8b567a5f37710ce6b7a
Will Lu <zeren828@gmail.com>
2017-08-08 14:51:27 +0800

    dev

--- application/controllers/crontab/Votes.php
+++ application/controllers/crontab/Votes.php
@@ -2,9 +2,39 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
+	private $data_debug;
+	private $data_result;
+	public function __construct() {
+		parent::__construct ();
+		$this->data_debug = true;
+		// 效能檢查
+		$this->output->enable_profiler(TRUE);
+	}
+	public function __destruct() {
+		parent::__destruct ();
+		unset ( $this->data_debug );
+		unset ( $this->data_result );
+	}
 	// 統計
 	public function statistics()
 	{
-		echo '統計';
+		try {
+			// 開始時間標記
+			$this->benchmark->mark ( 'code_start' );
+			// 引入
+			$this->config->load ( 'restful_status_code' );
+			$this->lang->load ( 'restful_status_lang', 'traditional-chinese' );
+			$this->load->driver ( 'cache', array (
+					'adapter' => 'memcached',
+					'backup' => 'dummy' 
+			) );
+			// 結束時間標記
+			$this->benchmark->mark ( 'code_end' );
+			// 標記時間計算
+			$this->data_result ['time'] = $this->benchmark->elapsed_time ( 'code_start', 'code_end' );
+			print_r($this->data_result);
+		} catch ( Exception $e ) {
+			show_error ( $e->getMessage () . ' --- ' . $e->getTraceAsString () );
+		}
 	}
 }
