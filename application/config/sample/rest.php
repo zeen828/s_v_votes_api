b942f7ec5863c1f23cce7f02844b290efb04783f
Will Lu <zeren828@gmail.com>
2017-08-08 10:13:39 +0800

    add 排程用

--- application/config/sample/rest.php
+++ application/config/sample/rest.php
@@ -365,7 +365,8 @@ $config['rest_key_length'] = 40;
 | 2012/06/12. See RFC 6648 specification for more details
 |
 */
-$config['rest_key_name'] = 'X-API-KEY';
+//$config['rest_key_name'] = 'X-API-KEY';
+$config['rest_key_name'] = 'Authorization';
 
 /*
 |--------------------------------------------------------------------------
---
+++ application/controllers/crontab/Votes.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends CI_Controller {
+	// 統計
+	public function statistics()
+	{
+		echo '統計';
+	}
+}
