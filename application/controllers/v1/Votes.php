2877cf22f8e4f97aea1782b17cf1aee2081d5041
Will Lu <zeren828@gmail.com>
2017-08-09 14:50:23 +0800

    dev

--- application/controllers/v1/Votes.php
+++ application/controllers/v1/Votes.php
@@ -130,7 +130,7 @@ class Votes extends MY_REST_Controller {
 			}
 			// 有無過期
 			$date_config = $data_cache [$data_cache ['config_name']];
-			if ( $data_input ['now_datetime'] < $date_config->start || $data_input ['now_datetime'] > $date_config->end) {
+			if ( $data_input ['now_datetime'] < $date_config->start || $data_input ['now_datetime'] > $date_config->end ) {
 				// 活動尚未開始
 				$this->data_result ['message'] = $this->lang->line ( 'permissions_error' );
 				$this->data_result ['code'] = $this->config->item ( 'permissions_error' );
