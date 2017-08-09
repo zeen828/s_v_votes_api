f0a6d6df9605bb5de080e385983b82d13a7a23fb
Will Lu <zeren828@gmail.com>
2017-08-09 11:24:31 +0800

    dev

--- application/models/postgre/Token_model.php
+++ application/models/postgre/Token_model.php
@@ -38,5 +38,18 @@ class Token_model extends CI_Model {
 		}
 		return false;
 	}
-
+	public function get_user_row_by_token($select, $token)
+	{
+		if (! empty ( $select )) {
+			$this->r_db->select ( $select );
+		}
+		$this->r_db->where ( 'token', $token );
+		$this->r_db->join('identities', 'identities.id = oauth_access_tokens.resource_owner_id', 'left');
+		$query = $this->r_db->get ( 'oauth_access_tokens' );
+		// echo $this->r_db->last_query();
+		if ($query->num_rows () > 0) {
+			return $query->row ();
+		}
+		return false;
+	}
 }
