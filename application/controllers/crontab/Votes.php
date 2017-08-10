aa41d61a7ba84c42fb5149d61b2e7a953a90d613
Will Lu <zeren828@gmail.com>
2017-08-10 16:11:18 +0800

    dev

--- application/controllers/crontab/Votes.php
+++ application/controllers/crontab/Votes.php
@@ -21,6 +21,38 @@ class Votes extends CI_Controller {
 	}
 	// 統計
 	public function statistics() {
+		try {
+			// 開始時間標記
+			$this->benchmark->mark ( 'code_start' );
+			// 引入
+			$this->load->model ( 'vidol_event/event_vote_config_model' );
+			$this->load->model ( 'vidol_event/event_vote_item_model' );
+			$this->load->driver ( 'cache', array (
+					'adapter' => 'memcached',
+					'backup' => 'dummy'
+			) );
+			// 變數
+			$vote_config = array ();
+			$data_cache = array ();
+			// 取得所有活動設定
+			$query = $this->event_vote_config_model->get_query_by_status_at ( '*' );
+			if ($query->num_rows () > 0) {
+				foreach ( $query->result () as $row ) {
+					$vote_config [$row->id] = $row;
+					unset ( $row );
+				}
+			}
+			unset ( $query );
+			//
+			unset ( $data_cache );
+			unset ( $vote_config );
+			// 結束時間標記
+			$this->benchmark->mark ( 'code_end' );
+			// 標記時間計算
+			$this->data_result ['time'] = $this->benchmark->elapsed_time ( 'code_start', 'code_end' );
+		} catch ( Exception $e ) {
+			show_error ( $e->getMessage () . ' --- ' . $e->getTraceAsString () );
+		}
 	}
 	// 建立暫存
 	public function cached() {
