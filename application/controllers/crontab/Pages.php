0fde15246283b8c9c15d86a4fa44433ab03e0b7e
Will Lu <zeren828@gmail.com>
2017-09-06 15:51:43 +0800

    load_page改名稱landing_page

--- application/controllers/crontab/Pages.php
+++ application/controllers/crontab/Pages.php
@@ -20,12 +20,12 @@ class Pages extends CI_Controller {
 		unset ( $this->data_result );
 	}
 	// 建立暫存
-	public function load_cached() {
+	public function landing_cached() {
 		try {
 			// 開始時間標記
 			$this->benchmark->mark ( 'code_start' );
 			// 引入
-			$this->load->model ( 'vidol_event/page_load_model' );
+			$this->load->model ( 'vidol_event/page_landing_model' );
 			$this->load->driver ( 'cache', array (
 					'adapter' => 'memcached',
 					'backup' => 'dummy' 
@@ -33,8 +33,8 @@ class Pages extends CI_Controller {
 			// 變數
 			$data_cache = array ();
 			// cache name key
-			$data_cache ['name'] = sprintf ( '%s_load_page', ENVIRONMENT );
-			$query = $this->page_load_model->get_query_limit ( '*', '30' );
+			$data_cache ['name'] = sprintf ( '%s_landing_page', ENVIRONMENT );
+			$query = $this->page_landing_model->get_query_limit ( '*', '30' );
 			if ($query->num_rows () > 0) {
 				foreach ( $query->result () as $row ) {
 					$data_cache [$data_cache ['name']] [$row->position] [] = array (
@@ -59,5 +59,44 @@ class Pages extends CI_Controller {
 			show_error ( $e->getMessage () . ' --- ' . $e->getTraceAsString () );
 		}
 	}
-	//dev
+	// 建立暫存
+	public function load_cached() {
+		try {
+			// 開始時間標記
+			$this->benchmark->mark ( 'code_start' );
+			// 引入
+			$this->load->model ( 'vidol_event/page_load_model' );
+			$this->load->driver ( 'cache', array (
+					'adapter' => 'memcached',
+					'backup' => 'dummy'
+			) );
+			// 變數
+			$data_cache = array ();
+			// cache name key
+			$data_cache ['name'] = sprintf ( '%s_load_page', ENVIRONMENT );
+			$query = $this->page_load_model->get_query_limit ( '*', '30' );
+			if ($query->num_rows () > 0) {
+				foreach ( $query->result () as $row ) {
+					$data_cache [$data_cache ['name']] [$row->position] [] = array (
+							'id' => $row->id,
+							'title' => $row->title,
+							'des' => $row->des,
+							'image' => $row->image,
+							'url' => $row->url
+					);
+				}
+			}
+			// 紀錄
+			$status = $this->cache->memcached->save ( $data_cache ['name'], $data_cache [$data_cache ['name']], 90000 );
+			$info = $this->cache->memcached->cache_info ();
+			//
+			unset ( $data_cache );
+			// 結束時間標記
+			$this->benchmark->mark ( 'code_end' );
+			// 標記時間計算
+			$this->data_result ['time'] = $this->benchmark->elapsed_time ( 'code_start', 'code_end' );
+		} catch ( Exception $e ) {
+			show_error ( $e->getMessage () . ' --- ' . $e->getTraceAsString () );
+		}
+	}
 }
