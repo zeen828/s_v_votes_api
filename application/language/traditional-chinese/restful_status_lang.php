f85f7e0f94b57f54f4f9f78293da5f06d39a07fe
Will Lu <zeren828@gmail.com>
2017-09-22 17:44:56 +0800

    增加錯誤訊息

--- application/language/traditional-chinese/restful_status_lang.php
+++ application/language/traditional-chinese/restful_status_lang.php
@@ -2,7 +2,7 @@
 $lang['system_default'] = '系統預設碼';
 $lang['system_success'] = '成功';
 $lang['system_error'] = '錯誤';
-$lang['system_not_config'] = '查無設頂檔';
+$lang['system_not_config'] = '查無設定檔';
 $lang['system_time_out'] = '操作超時';
 $lang['system_expired'] = '過期';
 
@@ -16,6 +16,7 @@ $lang['database_add_to_cancel_cash_error'] = '訂單失敗錯誤';
 $lang['database_check_rights_error'] = '判別使用者是否可觀看影片錯誤';
 $lang['database_not_data'] = '沒有資料';
 $lang['database_format_error'] = '資料格式錯誤';
+$lang['database_full_error'] = '名額已額滿';
 
 $lang['permissions_error'] = '權限錯誤';
 $lang['permissions_rest_error'] = 'REST KEY錯誤';
--- application/models/vidol_event/Event_vote_select_model.php
+++ application/models/vidol_event/Event_vote_select_model.php
@@ -42,4 +42,104 @@ class Event_vote_select_model extends CI_Model {
 		}
 		return false;
 	}
+	//取得會員資料
+	public function get_user_by_condifid_date($select, $config_id, $start_date=null, $end_date=null) {
+		if (! empty ( $select )) {
+			$this->r_db->select ( $select );
+		}
+		$this->r_db->where ( 'config_id', $config_id );
+		if(empty($start_date)){
+			$this->r_db->where ( 'created_at >=', $start_date );
+		}
+		if(empty($end_date)){
+			$this->r_db->where ( 'created_at <', $end_date );
+		}
+		$query = $this->r_db->get ( $this->table_name );
+		return $query;
+	}
+	// 投票數
+	public function get_vote_count_by_configid_date($config_id, $start_date, $end_date) {
+		$this->r_db->where ( 'config_id', $config_id );
+		$this->r_db->where ( 'data_no', '0' );
+		$this->r_db->where ( 'created_at >=', $start_date );
+		$this->r_db->where ( 'created_at <', $end_date );
+		$this->r_db->from ( $this->table_name );
+		$count = $this->r_db->count_all_results ();
+		// echo $this->r_db->last_query ();
+		return $count;
+	}
+	// 第一次投票會員
+	public function get_new_vote_count_by_configid_date($config_id, $start_date, $end_date) {
+		// 子查詢
+		$this->r_db->select ( '*, count(id) as count_no' );
+		$this->r_db->where ( 'config_id', $config_id );
+		$this->r_db->where ( 'data_no', '0' );
+		$this->r_db->where ( 'created_at <', $end_date );
+		$this->r_db->group_by ( 'user_id' );
+		$this->r_db->order_by ( 'created_at', 'ASC' );
+		$sql = $this->r_db->get_compiled_select ( $this->table_name );
+		// $sql = sprintf("SELECT count(*) FROM ( %s ) as t WHERE count_no = 1 AND created_at >= '%s' AND created_at < '%s' ", $sql, $start_date, $end_date);
+		// echo $sql;
+		$this->r_db->where ( 'count_no', '1' );
+		$this->r_db->where ( 'created_at >=', $start_date );
+		$this->r_db->where ( 'created_at <', $end_date );
+		$this->r_db->from ( '(' . $sql . ') as t ' );
+		$count = $this->r_db->count_all_results ();
+		// echo $this->r_db->last_query ();
+		return $count;
+	}
+	// 累計不重複投票數
+	public function get_single_vote_count_by_configid_date($config_id, $start_date, $end_date) {
+		// 子查詢
+		$this->r_db->select ( '*, count(id) as count_no' );
+		$this->r_db->where ( 'config_id', $config_id );
+		$this->r_db->where ( 'data_no', '0' );
+		$this->r_db->where ( 'created_at <', $end_date );
+		$this->r_db->group_by ( 'user_id' );
+		$this->r_db->order_by ( 'created_at', 'ASC' );
+		$this->r_db->from ( $this->table_name );
+		$count = $this->r_db->count_all_results ();
+		// echo $this->r_db->last_query ();
+		return $count;
+	}
+	// 累計投票數
+	public function get_total_vote_count_by_configid_date($config_id, $start_date, $end_date) {
+		$this->r_db->where ( 'config_id', $config_id );
+		$this->r_db->where ( 'data_no', '0' );
+		// $this->r_db->where ( 'created_at >=', $start_date );
+		$this->r_db->where ( 'created_at <', $end_date );
+		$this->r_db->from ( $this->table_name );
+		$count = $this->r_db->count_all_results ();
+		// echo $this->r_db->last_query ();
+		return $count;
+	}
+	// 投票註冊數
+	public function get_registered_count_by_configid_date($config_id, $start_date, $end_date) {
+		// 子查詢
+		$this->r_db->select ( 'datediff(`created_at`, date_add(`user_created_at`, interval 8 hour)) as user_date' );
+		$this->r_db->where ( 'config_id', $config_id );
+		$this->r_db->where ( 'data_no', '0' );
+		$this->r_db->where ( 'created_at >=', $start_date );
+		$this->r_db->where ( 'created_at <', $end_date );
+		$sql = $this->r_db->get_compiled_select ( $this->table_name );
+		$this->r_db->where ( 'user_date', '0' );
+		$this->r_db->from ( '(' . $sql . ') as t ' );
+		$count = $this->r_db->count_all_results ();
+		// echo $this->r_db->last_query ();
+		return $count;
+	}
+	// 累計投票註冊數
+	public function get_total_registered_count_by_configid_date($config_id, $start_date, $end_date) {
+		// 子查詢
+		$this->r_db->select ( 'datediff(`created_at`, date_add(`user_created_at`, interval 8 hour)) as user_date' );
+		$this->r_db->where ( 'config_id', $config_id );
+		$this->r_db->where ( 'data_no', '0' );
+		$this->r_db->where ( 'created_at <', $end_date );
+		$sql = $this->r_db->get_compiled_select ( $this->table_name );
+		$this->r_db->where ( 'user_date', '0' );
+		$this->r_db->from ( '(' . $sql . ') as t ' );
+		$count = $this->r_db->count_all_results ();
+		// echo $this->r_db->last_query ();
+		return $count;
+	}
 }
